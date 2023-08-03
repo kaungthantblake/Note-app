a2adb6d1b7669930b42829c7bd50df37b34c423e
kaungthantblake <shido1812@gmail.com>
2023-08-03 14:50:35 +0630

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'note_APP';
+
+
+  onFileChange(event:any){
+    const file = event.target.file[0]
+    if(file){
+      console.log(file);
+      
+    }
+
+  }
 }
